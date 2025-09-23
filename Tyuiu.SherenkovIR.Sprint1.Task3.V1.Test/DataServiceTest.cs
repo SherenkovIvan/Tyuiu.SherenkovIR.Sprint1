7228e67a2ad379aac944d605cbf4236581fd4bf6
Sherenkov Ivan <sherenkov12@gmail.com>
2025-09-23 16:21:32 +0500

    Task3

---
+++ Tyuiu.SherenkovIR.Sprint1.Task3.V1.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.SherenkovIR.Sprint1.Task3.V1.Lib;
+namespace Tyuiu.SherenkovIR.Sprint1.Task3.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double r = 4.32;
+            double h = 1.23;
+            double wait = 5.314;
+            var res = ds.CylinderVolume(r, h);
+            Assert.AreEqual(wait , res);
+        }
+    }
+}
---
+++ Tyuiu.SherenkovIR.Sprint1.Task3.V1/Program.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.SherenkovIR.Sprint1.Task3.V1.Lib;
+DataService ds = new DataService();
+
+Console.Title = "Спринт #1| Выполнил: Шеренков И. Р. | ИБКСБ-25-1";
+// Длинна строки 75 символов
+Console.WriteLine("****************************************************");
+Console.WriteLine("* Спринт #1                                        *");
+Console.WriteLine("* Тема: Организация ввода и вывода в консольных    *");
+Console.WriteLine("* приложениях.                                     *");
+Console.WriteLine("* Задание #3                                       *");
+Console.WriteLine("* Вариант #1                                     *");
+Console.WriteLine("* Выполнил: Шеренков Иван Романович | ИБКСБ-25-1   *");
+Console.WriteLine("****************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                         *");
+Console.WriteLine("* Написать программу, которая запрашивает у        *");
+Console.WriteLine("* пользователя исходные данные, выполняет          *");
+Console.WriteLine("* указанные расчеты и печатает результат на экране *");
+Console.WriteLine("*                                                  *");
+Console.WriteLine("****************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                 *");
+Console.WriteLine("****************************************************");
+
+double r = 4.32;
+double h = 1.23;
+Console.WriteLine("Сторона H цилиндра = " + h);
+Console.WriteLine("Сторона R цилиндра = " + r);
+
+Console.WriteLine("****************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                       *");
+Console.WriteLine("****************************************************");
+
+Console.WriteLine("ОбЪем цилиндра = " + ds.CylinderVolume(r, h));
+Console.ReadKey();
