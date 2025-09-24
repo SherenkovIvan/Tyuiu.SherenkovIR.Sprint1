fe9c42e10f7909e1255974629e24507ae4b62993
Sherenkov Ivan <sherenkov12@gmail.com>
2025-09-24 16:41:44 +0500

    Task5

---
+++ Tyuiu.SherenkovIR.Sprint1.Task5.V1.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.SherenkovIR.Sprint1.Task5.V1.Lib;
+namespace Tyuiu.SherenkovIR.Sprint1.Task5.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            double x1 = 10;
+            double y1 = 20;
+            double x2 = 30;
+            double y2 = 40;
+            DataService ds = new DataService();
+            double res = ds.DistanceBetweenDots(x1, y1, x2, y2);
+            int result = Convert.ToInt32(res);
+            int wait = 28;
+            Assert.AreEqual((double)wait, (double)result, 1.0);
+            
+
+        }
+    }
+}
---
+++ Tyuiu.SherenkovIR.Sprint1.Task5.V1/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.SherenkovIR.Sprint1.Task5.V1.Lib;
+
+DataService ds = new DataService();
+Console.Title = "Спринт #1| Выполнил: Шеренков И. Р. | ИБКСБ-25-1";
+// Длинна строки 75 символов
+Console.WriteLine("****************************************************");
+Console.WriteLine("* Спринт #1                                        *");
+Console.WriteLine("* Тема: Преобразование типов и класс Convert       *");
+Console.WriteLine("* Задание #5                                       *");
+Console.WriteLine("* Вариант #1                                       *");
+Console.WriteLine("* Выполнил: Шеренков Иван Романович | ИБКСБ-25-1   *");
+Console.WriteLine("****************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                         *");
+Console.WriteLine("* Написать программу, которая следующую задачу     *");
+Console.WriteLine("* Найти расстояние между двумя точками с заданными *");
+Console.WriteLine("* координатами (x, y). Ответ привести к целому     *");
+Console.WriteLine("* с помощью класса Convert.                        *");
+Console.WriteLine("*                                                  *");
+Console.WriteLine("****************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                 *");
+Console.WriteLine("****************************************************");
+
+Console.WriteLine("Введите значение x1:");
+double x1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите значение y1:");
+double y1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите значение x2:");
+double x2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите значение y2:");
+double y2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("****************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                       *");
+Console.WriteLine("****************************************************");
+
+int res = Convert.ToInt32(ds.DistanceBetweenDots(x1, y1, x2, y2));
+Console.WriteLine(res);
+
+Console.ReadKey();
